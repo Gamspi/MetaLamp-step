9b6dd933efbfac8c633926f2b65609a988d3c404
Gamspi <niko.tatt007@gmail.com>
2022-04-05 10:58:09 +0700

    Настройка WebPack

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require("path")
+const HTMLWebpackPlugin = require("html-webpack-plugin")
+const {CleanWebpackPlugin} = require("clean-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin');
+
+const isDev = process.env.NODE_ENV === "development"
+const isProd = !isDev
+
+
+module.exports = {
+    mode: "development",
+    context: path.resolve(__dirname, "src"),
+    entry: './index.js',
+    output: {
+        filename: "[name].[hash].js",
+        path: path.resolve(__dirname, "dist")
+    },
+    devServer: {
+        port: 3000,
+        hot: isDev
+    },
+    optimization: {
+        splitChunks: {
+            chunks: "all"
+        }
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "index.pug",
+            minify: {
+                collapseWhitespace: isProd
+            }
+
+        }),
+        new HtmlWebpackPugPlugin(),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "[name].[hash].css"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {},
+                    },
+                    "css-loader",
+                    "sass-loader"]
+            },
+            {
+                test: /\.(jpg|jpeg|png|svg)$/,
+                use: ["file-loader"]
+            },
+        ]
+    }
+}
